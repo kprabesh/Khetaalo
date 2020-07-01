01b5e6d57ed67afe801e28e63d76b0b7a4af0195
Elkale <prabesh.17@gmail.com>
2020-06-30 22:20:00 -0700

    form handler

---
+++ review.php
@@ -0,0 +1,19 @@
+﻿<?php
+
+if(isset($_POST['submit'])) {
+ $name = $_POST['name'];
+ $mailfrom=$_POST['email'];
+ $message=$_POST['message'];
+
+ $mailTo='koirala.prabesh@outlook.com';
+ $headers="From: ".$mailfrom;
+ $txt="You have received a review from".$name."\n\".$message;
+ 
+
+
+ mail($mailTo,$message,$headers);
+ header("Location:index.html?mailsend");
+
+}
+
+?>
